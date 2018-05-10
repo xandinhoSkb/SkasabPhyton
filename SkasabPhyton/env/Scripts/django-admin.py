353710fe36c282feb7af8e39d950d67d5fc5cad2
xandinhoSkb <xandihobrasil2017@gmail.com>
2018-05-10 07:19:45 -0300

    Adicionar arquivos de projeto.

---
+++ SkasabPhyton/env/Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!C:\Users\xande\source\repos\SkasabPhyton\SkasabPhyton\env\Scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
